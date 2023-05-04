c8872ec73c817f59bd97e98d67bd759457e920d4
Crowdbotics <team@crowdbotics.com>
2023-05-04 18:26:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-petopia-layout-40457",
+  url: "https://test-petopia-layout-40457.botics.co",
+  api: "https://test-petopia-layout-40457.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
